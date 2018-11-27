61c0db1d302bad0cbf9974d08bb1c0a8e13ee1f6
Nhan Doan <doanvannhan1226@gmail.com>
2018-11-27 17:19:11 +0700

    up code

--- Shopping.Domain/Migrations/Configuration.cs
+++ Shopping.Domain/Migrations/Configuration.cs
@@ -4,6 +4,7 @@ namespace Shopping.Domain.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Shopping.Domain.Entities;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Shopping.Domain.Concetre.DemoContext>
     {
@@ -12,12 +13,27 @@ namespace Shopping.Domain.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(Shopping.Domain.Concetre.DemoContext context)
+        protected override void Seed(Concetre.DemoContext context)
         {
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+            var category = new Category[]
+              {
+            new Category{CategoryName ="Category 1"},
+            new Category{CategoryName ="Category 2"},
+            new Category{CategoryName ="Category 3"},
+            new Category{CategoryName ="Category 4"},
+            new Category{CategoryName ="Category 5"},
+
+
+              };
+            foreach (Category c in category)
+            {
+                context.Categories.AddOrUpdate(c);
+            }
+            context.SaveChanges();
         }
     }
 }
